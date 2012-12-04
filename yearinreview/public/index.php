0ec72d502923cf02a48bf34f70f0b053bd9f0085
Peter Meth <pmeth@centah.com>
2012-12-04 12:16:00 -0500

    filling in slides

--- yearinreview/public/index.php
+++ yearinreview/public/index.php
@@ -7,12 +7,20 @@ $view = array(
 $slides = array(
 	'What happened in 2012?',
 	'10 Meetups<br>35 attendees average',
-	'Membership grew from 221 to over 530<br><img width="800" height="600" src="images/size.png"',
-	'<h2>PRIZES</h2>
-	<ul>
-		<li>#1 - Jetbrains License (Any product)</li>
-		<li>#2 - Virtual Conference Ticket</li>
-	</ul>',
+	'Membership grew from 221 to over 540<br><img width="800" height="488" src="images/size.png">',
+	'40 new members in November<br><img width="800" height="487" src="images/growth.png">',
+	'JUNE<br><br>I joined the organizing committee',
+	'SEPTEMBER<br><br>Celebrated our 2 Year Anniversary',
+	'OCTOBER<br><br>Moved to Instaclick',
+	'NOVEMBER<br><br>True North PHP Conference',
+	'What\'s up for 2013?',
+	'2 x 30 Presentation Format',
+	'Hackathons',
+	'Weekend Workshops',
+	'Training Courses',
+	'GTAPHP North',
+	'True North PHP #2',
+	'PRIZES<br><br>#1 - Jetbrains License (Any product)<br>#2 - Virtual Conference Ticket',
 	'<a href="winnergenerator">Winner Generator</a>',
 );
 
